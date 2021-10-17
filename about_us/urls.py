e5da052ccb88d3b48a18aab6e8c11cf0e6f1af7f
naomikalfa <naomikalfa.x@gmail.com>
2021-10-16 21:38:07 -0400

    Add files via upload

--- about_us/urls.py
+++ about_us/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls import include, url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    url(r'^musicplayer/', include(('musicplayer.urls', 'musicplayer'))),
 ]
--- about_us/views.py
+++ about_us/views.py
@@ -2,25 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
+
     context = {'template': 'about_us',
-               'title': 'About Us',
-               'description': 'We present to you Argophonica, a site to sate your cochleae!\n\n'
-                              'Whence comes our name? Read on, fair listener.\n\n'
-                              '\nIn "Metamporphoses" Book 11, Ovid writes of Orpheus: \n'
-                              '"With his songs, Orpheus, the bard of Thrace, allured the trees, the savage animals, '
-                              'and even the insensate rocks, to follow him."\n'         
-                              '\n+\n\n'
-                              'Before the Latin writings came the Greek writings.\n'
-                              'One of its epics, "Argonautica Orphica", is narrated by the voice of Orpheus. It is '
-                              'herein claimed that the Argo was the first ship ever built. It had many splendorous '
-                              '& terrifying adventures, one of which was its encounter with the overwhelming sounds ' 
-                              'of the Sirens, as the Argonauts carried out their quest to bring the Golden Fleece to '
-                              'Iolcus.\n'
-                              '\n+\n\n'
-                              'The adjective "phonic", denoting that which pertains to sound, comes to us through the '
-                              'Greek phōnē which denotes sound or voice.\n'
-                              '\n=\n\n'
-                              'Argophonica!\n\n'
-                              '\nMay the music be as a voyage to you, a quest, an epic to traverse!\n\n'}
+               'title': 'About Us'}
 
     return render(request, 'about_us/index.html', context)
