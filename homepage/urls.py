aa928b6a3ab6b5abdb596a7e84dd3251f5a5fca8
naomikalfa <naomikalfa.x@gmail.com>
2021-10-16 21:41:06 -0400

    Add files via upload

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,10 @@
+from django.conf.urls import include, url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    url(r'^musicplayer/', include(('musicplayer.urls', 'musicplayer'))),
 ]
 
